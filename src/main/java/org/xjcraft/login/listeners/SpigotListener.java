03df04326fdab60d15f50d01acd9eb8b07411fd0
Ree <reezhuwy@gmail.com>
2026-08-25 00:52:37 +0800

    bump version to 26.2

--- src/main/java/org/xjcraft/login/listeners/SpigotListener.java
+++ src/main/java/org/xjcraft/login/listeners/SpigotListener.java
@@ -3,6 +3,7 @@ package org.xjcraft.login.listeners;
 import com.google.common.io.ByteArrayDataInput;
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
+import lombok.extern.slf4j.Slf4j;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -13,11 +14,15 @@ import org.xjcraft.login.Spigot;
 import org.xjcraft.login.bean.Account;
 import org.xjcraft.login.manager.Manager;
 
+import java.util.*;
+
 import static org.xjcraft.login.bean.Constant.CHANNEL;
 
+@Slf4j
 public class SpigotListener implements Listener, PluginMessageListener {
     private Spigot plugin;
     private final Manager manager;
+    private final Map<String, List<String>> duplicates = new HashMap<>();
 
     public SpigotListener(Spigot plugin, Manager manager) {
         this.plugin = plugin;
@@ -46,6 +51,19 @@ public class SpigotListener implements Listener, PluginMessageListener {
         Account account = manager.getCachedAccount(name);
         if (account.getHide()) return;
 //        message = message.substring(1);
+        List<String> old = duplicates.computeIfAbsent(name, s -> new ArrayList<>());
+        boolean duplicate = old.stream().anyMatch(s -> Objects.equals(s, message));
+
+        if (duplicate) {
+            event.getPlayer().sendMessage("请勿发送重复的消息！");
+            log.info("player {} fail to send message due to duplicate", event.getPlayer().getName());
+            return;
+        } else {
+            old.add(message);
+            if (old.size() > 20) {
+                old.remove(0);
+            }
+        }
         ByteArrayDataOutput out = ByteStreams.newDataOutput();
         out.writeUTF(name);
         out.writeUTF(message);
